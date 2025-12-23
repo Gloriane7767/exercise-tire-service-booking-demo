642400d57484d2aff963a1f300a88c281be0c2e9
Glory Ngum Foncha <gloriane7767@gmail.com>
2025-12-23 11:42:45 +0100

    create service class

--- src/main/java/com/gloriane/Service.java
+++ src/main/java/com/gloriane/Service.java
@@ -1,4 +1,25 @@
 package com.gloriane;
 
-public class Service {
-}
+public abstract class Service {
+
+    protected int id;
+    protected String name;
+    protected double basePrice;
+
+    public Service(int id, String name, double basePrice) {
+        this.id = id;
+        this.name = name;
+        this.basePrice = basePrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
