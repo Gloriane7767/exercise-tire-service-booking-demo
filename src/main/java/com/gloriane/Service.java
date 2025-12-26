a6f915c26986b742e2890be1a70f919e0074bcdf
Glory Ngum Foncha <gloriane7767@gmail.com>
2025-12-26 14:26:29 +0100

    update service class  with methods to calculate price and duration.

--- src/main/java/com/gloriane/Service.java
+++ src/main/java/com/gloriane/Service.java
@@ -1,17 +1,20 @@
 package com.gloriane;
 
 public abstract class Service {
-
+    // These variables belong to ALL services
     private int id;
     private String name;
     private double basePrice;
 
+
+    // Constructor: used when a child class is created
     public Service(int id, String name, double basePrice) {
         this.id = id;
         this.name = name;
         this.basePrice = basePrice;
     }
 
+    // Getters: allow safe access to private data
     public int getId() {
         return id;
     }
@@ -23,4 +26,10 @@ public abstract class Service {
     public double getBasePrice() {
         return basePrice;
     }
+
+    // Every service MUST tell its price
+    public abstract double calculatePrice();
+
+    // Every service MUST tell how long it takes
+    public abstract int getDurationInMinutes();
 }
